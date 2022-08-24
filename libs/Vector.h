682d25a6fe160b04ef05c992aa6fcbb072612ff4
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2022-08-24 12:09:17 +0300

    Rotate vector abbility and add destructor
    A method has been added to rotate the vector, and a class destructor has also been added.

--- libs/Vector.h
+++ libs/Vector.h
@@ -2,68 +2,89 @@
 #define __VECTOR_H__
 
 #include "iostream"
+#include "math.h"
+
+#define PI 3.14
 
 template <typename T>
 class Vector2 {
     public:
 
+    /* Constructor section */
     Vector2();
     Vector2(T x, T y);
+
+    /*  Destructor section  */
+    ~Vector2() = default;
 
     void operator + (const Vector2 &other); 
     void operator * (int factor); 
     void operator = (const Vector2 &other);
     
     static void Output(const Vector2 currentVec);
+    Vector2<T> Rotate(float angle);
 
     private: 
 
-    T _a;
-    T _b;
+    T _x;
+    T _y;
 };
 
 
 template <typename T>
 Vector2<T>::Vector2()
 {
-    _a = 0;
-    _b = 0;    
+    _x = 0;
+    _y = 0;    
 };
 
 template<typename T>
 Vector2<T>::Vector2(T x, T y)
 {
-    _a = x;
-    _b = y;    
+    _x = x;
+    _y = y;    
 }
 
 template<typename T>
 void Vector2<T>::operator * (int factor)
 {
-    _a *= factor;
-    _b *= factor;  
+    _x *= factor;
+    _y *= factor;  
 }
 
 template<typename T>
 void Vector2<T>::operator + (const Vector2 &other)
 {
-    _a += other._a;
-    _b += other._b; 
+    _x += other._x;
+    _y += other._y; 
 }
 
 template <typename T>
 void Vector2<T>::operator = (const Vector2 &other)
 {
-    _a = other._a;
-    _b = other._b;
+    _x = other._x;
+    _y = other._y;
 }
 
 template <typename T>
 void Vector2<T>::Output(const Vector2 currentVec)
 {
-    std::cout << "Vector start: " + std::to_string(currentVec._a);
+    std::cout << "Vector start: " + std::to_string(currentVec._x);
     std::cout << "\n";
-    std::cout << "Vector end: " + std::to_string(currentVec._b) + "\n"; 
+    std::cout << "Vector end: " + std::to_string(currentVec._y) + "\n"; 
+}
+
+template <typename T> 
+Vector2<T> Vector2<T>::Rotate(float angle) {
+    angle = fmod(angle, 360.0);
+    angle = ((angle) * PI / 180.0f);
+    float sinAngle = sin(angle);
+    float cosAngle = cos(angle);
+    
+    float temporaryX = (cosAngle * _x - sinAngle * _y);
+    float temporaryY = (sinAngle * _x + cosAngle * _y);
+
+    return Vector2<T>(temporaryX, temporaryY);
 }
 
 #endif // __VECTOR_H__
